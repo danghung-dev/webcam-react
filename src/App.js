70d8801fcec5872109e38ccda0455c135d503bb2
Dang Hung <hung.dang980@gmail.com>
2019-11-27 09:51:45 +0700

    remove interval

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ class App extends React.Component {
     this.webcamRef = React.createRef()
     this.state ={
       userName: 'No face',
-      currentStatus: 1,
+      currentStatus: 2,
       currentQuality: 1,
      
     }
     this.deviceQuality= 1
   }
   componentDidMount (){
-    this.intervalId = setInterval(this.capture, 1000);
+    // this.intervalId = setInterval(this.capture, 1000);
   }
   componentWillUnmount (){
-    this.clearInterval()
+    // this.clearInterval()
   }
   pressButtonStatus = ()=>{
     const {currentStatus} = this.state
@@ -95,10 +95,10 @@ class App extends React.Component {
          </div>
         <p style={{fontSize: 40, color: '#1303fc', fontWeight: 'bold', marginBlockStart: '10px'}}> {userName}</p>
         {currentStatus === 2 ? <button style={{height: 50, width: 200, marginTop: 20, backgroundColor: '#0a802b', color: 'white', fontWeight: 'bold'}} onClick={this.capture}> CHỤP</button> : null}
-         <button 
+         {/* <button 
             style={{height: 50, width: 200, marginTop: 20, backgroundColor: '#f65335', color: 'white', fontWeight: 'bold'}} onClick={this.pressButtonStatus}> 
             {currentStatus === 1 ? 'BẬT CHẾ ĐỘ THỦ CÔNG': 'BẬT CHẾ ĐỘ AUTO'}
-         </button>
+         </button> */}
        
       </div>
     );
